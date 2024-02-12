508bde335d5c21cac81c8623ad060708b1abf49f
FifalianaRabary <158579041+FifalianaRabary@users.noreply.github.com>
2024-02-12 17:52:53 +0300

    front login admin
    front login admin

---
+++ pages/loginAdmin.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="cssLoginAdmin.css" rel="stylesheet">
+</head>
+<body>
+
+
+  
+    <div class="container col-xs-12 col-md-12 col-sm-12 col-lg-12">
+        
+        
+        <div class="text col-xs-7 col-md-7 col-sm-7 col-lg-7">
+            <h1 class="bienvenu">Welcome to</h1>
+            <h1 class="tea-ko">Tea-ko</h1>
+        </div>
+
+        <div class="divLogin col-xs-5 col-md-5 col-sm-5 col-lg-5">
+
+            <div class="formulaire col-xs-12 col-md-12 col-sm-12 col-lg-12">
+
+                <form action="" method="get">
+                    <label for="">Email:</label>
+                    <input type="email" name="mail" value="admin1@gmail.com">
+                    <br>
+                    <label for="">Password:</label>
+                    <input type="password" name="mdp" value="mdp1">
+                    <br>
+                    <input type="submit" class="login btn btn-primary" value="Log in">
+                    
+                </form>
+            
+            </div>
+            
+        </div>
+
+        
+
+    </div>
+
+    <footer >
+        <p>Rabary Mihaingo Soa Harena Fifaliana ETU002455</p>
+        <p>Rakotoharitiana Hedy Hamael ETU002500</p>
+        <p>Randriambololona Kaloina ETU002571</p>
+    </footer>
+
+    
+
+   
+   
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
